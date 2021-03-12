42ddd15b4f7df8bd58851b02cb118c98e738e0fb
natcza <natalia.nc.czajkowska@gmail.com>
2021-03-12 19:48:52 +0100

    12.03.2021

--- app1/forms.py
+++ app1/forms.py
@@ -1,15 +1,38 @@
 from django import forms
+from django.forms import formset_factory
+
 from app1.models import Topping
 
-#django Formset
-class ToppingForm(forms.Form):
-    toppings = Topping.objects.all()
-    i = 1
-    tab = []
-    for topping in toppings:
-        t = (i, topping.name + ", " + str(topping.price) + " zl" + "id=" + str(topping.pk))
-        i += 1
-        tab.append(t)
-    topp = forms.MultipleChoiceField(label="Dodatki", help_text="Mozesz wybrac dwa dodatki!",
-                                     widget=forms.CheckboxSelectMultiple, choices=tab)
-    # topp = forms.ChoiceField(label="Dodatki", widget=forms.RadioSelect, choices=tab, required=True)
+
+# django Formset
+# class ToppingForm(forms.Form):
+#     toppings = Topping.objects.all()
+#     i = 1
+#     tab = []
+#     for topping in toppings:
+#         t = (i, topping.name + ", " + str(topping.price) + " zl" + "id=" + str(topping.pk))
+#         i += 1
+#         tab.append(t)
+#     topp = forms.MultipleChoiceField(label="Dodatki", help_text="Mozesz wybrac dwa dodatki!",
+#                                      widget=forms.CheckboxSelectMultiple, choices=tab)
+#     # topp = forms.ChoiceField(label="Dodatki", widget=forms.RadioSelect, choices=tab, required=True)
+
+
+# class ToppingForm(forms.Form):
+#     topping = forms.ModelMultipleChoiceField(queryset=Topping.objects.all())
+
+#gdy dziedziczymy z modelform (kreowanie automatycznych formularzy dla modeli)
+    # class Meta:
+    #     model = Topping
+    #     fields = ('name',)
+
+class ToppingForm(forms.ModelForm):
+    number = forms.IntegerField()
+    is_active = forms.BooleanField()
+    name = forms.CharField(disabled=True)
+    class Meta:
+        model = Topping
+        fields = ['name', 'price']
+        # widgets = {
+        #     'name': Charfield(attrs={'cols': 80, 'rows': 20}),
+        # }
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,4 @@
+from django.forms import modelform_factory, modelformset_factory
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from app1.models import (
@@ -21,36 +22,58 @@ class PizzaView(View):
         return render(request, self.template_name, ctx)
 
 
+# class PizzaDetailsView(View):
+#     template_name = 'app1/pizza_details_view.html'
+#
+#     def get(self, request, *args, **kwargs):
+#         form = ToppingForm()
+#         message = None
+#         pizza_id = kwargs['pk']
+#         pizza = Pizza.objects.get(pk=pizza_id)
+#         toppings = Topping.objects.all()
+#
+#         ctx = {
+#             'pizza': pizza,
+#             'toppings': toppings,
+#             'message': message,
+#             'form': form,
+#         }
+#         return render(request, self.template_name, ctx)
+#
+#     def post(self, request, *args, **kwargs):
+# form = ToppingForm(request.POST)
+# if not form.is_valid():  # TODO if not_valid():
+#     return redirect('pizza-list')
+# pizza_id = kwargs['pk']
+# chosen_pizza = get_object_or_404(Pizza, pk=pizza_id)
+# #TODO skorzystac z metody sum musimy miec cene skladnikow plus cena pizzy
+# topp = form.cleaned_data.get('topp')
+# # order = Order.objects.create(order_price=, note)
+# ordered_pizza = Pizza.objects.create(name=chosen_pizza.name, size=chosen_pizza.size, price=chosen_pizza.price)
+# for i in topp:
+#     ordered_pizza.toppings.add(Topping.objects.get(pk=int(i)))
+# return redirect('pizza-list')
+
+
 class PizzaDetailsView(View):
     template_name = 'app1/pizza_details_view.html'
 
     def get(self, request, *args, **kwargs):
         form = ToppingForm()
+        ToppingFormSet = modelformset_factory(Topping, ToppingForm)
+        formset = ToppingFormSet(queryset=Topping.objects.all())
         message = None
         pizza_id = kwargs['pk']
         pizza = Pizza.objects.get(pk=pizza_id)
         toppings = Topping.objects.all()
-
-        ctx = {
-            'pizza': pizza,
-            'toppings': toppings,
-            'message': message,
+        context = {
             'form': form,
+            'pizza': pizza,
+            'formset': formset,
         }
-        return render(request, self.template_name, ctx)
+        return render(request, self.template_name, context)
 
-    def post(self, request, *args, **kwargs):
-        form = ToppingForm(request.POST)
-        if not form.is_valid():  # TODO if not_valid():
-            return redirect('pizza-list')
-        pizza_id = kwargs['pk']
-        chosen_pizza = get_object_or_404(Pizza, pk=pizza_id)
-        #TODO skorzystac z metody sum musimy miec cene skladnikow plus cena pizzy
-        topp = form.cleaned_data.get('topp')
-        order = Order.objects.create(order_price=, note)
-        ordered_pizza = Pizza.objects.create(name=chosen_pizza.name, size=chosen_pizza.size, price=chosen_pizza.price)
-        for i in topp:
-            ordered_pizza.toppings.add(Topping.objects.get(pk=int(i)))
-        return redirect('pizza-list')
-
+    # def post(self, request, *args, **kwargs):
+    #     form = ToppingForm(request.POST)
+    #     if form.is_valid():
 
